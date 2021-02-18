95e29f4d2d5f09daa547618191b305f8016521e2
Karin Liegmann <karin.liegmann@googlemail.com>
2021-02-18 11:33:34 +0100

    added uuid add-on

---
+++ wizard-shopping-list/src/Form/DeleteButton.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import Button from '../Button/Button'
+
+export default function DeleteButton({onDeleteAll}) {
+    <Button type="submit" text="Delete All" onClick={onDeleteAll}/>
+} 
